6cac4702100527a553f2df76514b368d2dfd6b8d
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-18 12:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+const h1 = document.getElementById("sizeInfo");
+window.onresize = function(){
+    w = window.innerWidth;
+    h = window.innerHeight;
+    h1.innerText = "Width: "+w +" and "+"Height:"+" "+h;
+    
+}
